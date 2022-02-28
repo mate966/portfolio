ad0efbd0504e1ccefded33de96642f4cb864e767
Mateusz Szweda <szweda.mateusz@wp.pl>
2022-02-28 17:58:40 +0100

    add htacces & google analytics, add sticky menu, change logo for img

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,20 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import Burger from "./Burger";
+import Logo from "../assets/Logo.png";
 
 const Nav = () => {
     const [isActive, setIsActive] = useState(false);
+    const [offSet, setOffSet] = useState(0);
+
+    const setScroll = () => {
+        setOffSet(window.scrollY);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", setScroll);
+    });
 
     return (
-        <div className="nav wrapper">
-            <div className="logo">
-                <div className="logo-rectangle"></div>
-                <div className="logo-initials">MS</div>
-            </div>
-            <nav className={!isActive ? "menu" : "menu active-menu"}>
-                <ul className="menu__list">
+        <nav
+            className={
+                offSet >= window.innerHeight - 50
+                    ? "navigation sticky"
+                    : "navigation"
+            }
+        >
+            <div className="navigation__container wrapper">
+                <HashLink smooth to="/#">
+                    <img src={Logo} alt="Logo portfolio" />
+                </HashLink>
+                <ul
+                    className={
+                        isActive ? "menu__list active-menu" : "menu__list"
+                    }
+                >
                     <li
                         className={
                             window.location.pathname === "/"
@@ -22,7 +40,9 @@ const Nav = () => {
                                 : "menu__list-element"
                         }
                     >
-                        <Link to="/">Strona Główna</Link>
+                        <HashLink smooth to="/#">
+                            Strona Główna
+                        </HashLink>
                     </li>
                     <li
                         className={
@@ -31,7 +51,6 @@ const Nav = () => {
                                 : "menu__list-element"
                         }
                     >
-                        {/* <Link to="/about">O mnie</Link> */}
                         <HashLink smooth to="/about/#about">
                             O mnie
                         </HashLink>
@@ -59,16 +78,70 @@ const Nav = () => {
                                 : "menu__list-element "
                         }
                     >
-                        {/* <Link to="/contact">Kontakt</Link> */}
                         <HashLink smooth to="/contact/#contact">
                             Kontakt
                         </HashLink>
                     </li>
                 </ul>
-            </nav>
-            <Burger isActive={isActive} setIsActive={setIsActive} />
-        </div>
+                <Burger isActive={isActive} setIsActive={setIsActive} />{" "}
+            </div>
+        </nav>
     );
 };
 
 export default Nav;
+
+{
+    /* <nav className={!isActive ? "menu" : "menu active-menu"}>
+    <ul className="menu__list">
+        <li
+            className={
+                window.location.pathname === "/"
+                    ? "menu__list-element active"
+                    : "menu__list-element"
+            }
+        >
+            <Link to="/">Strona Główna</Link>
+        </li>
+        <li
+            className={
+                window.location.pathname === "/about/"
+                    ? "menu__list-element active"
+                    : "menu__list-element"
+            }
+        >
+            <Link to="/about">O mnie</Link>
+            <HashLink smooth to="/about/#about">
+                O mnie
+            </HashLink>
+        </li>
+        <li className="menu__list-element">
+            <HashLink smooth to="/#skills">
+                Umiejętności
+            </HashLink>
+        </li>
+        <li
+            className={
+                window.location.pathname === "/portfolio/"
+                    ? "menu__list-element active"
+                    : "menu__list-element"
+            }
+        >
+            <HashLink smooth to="/portfolio/#portfolio">
+                Portfolio
+            </HashLink>
+        </li>
+        <li
+            className={
+                window.location.pathname === "/contact/"
+                    ? "menu__list-element active"
+                    : "menu__list-element "
+            }
+        >
+            <HashLink smooth to="/contact/#contact">
+                Kontakt
+            </HashLink>
+        </li>
+    </ul>
+</nav>; */
+}
